1e816102c02a183f35bf53f184e39951fb813faa
Kenneth <kennethyoung.2960@gmail.com>
2024-02-25 17:58:08 -0600

    worked on simplifying code in new function

--- practice1.js
+++ practice1.js
@@ -13,4 +13,11 @@ function printAuthorAndTitle(product) {
     return `${title} by ${author.firstName} ${author.lastName}`;
 }
 
+function printTitleAndPrice(product) {
+    const { title, priceInCents } = product;
+    return `${title} is available for $${(priceInCents / 100 ).toFixed(2)}`;
+}
+//prints title and price without multiple const declarations and in one line. Also reduces usage of product.
+
+console.log(printTitleAndPrice(book));
 console.log(printAuthorAndTitle(book));
