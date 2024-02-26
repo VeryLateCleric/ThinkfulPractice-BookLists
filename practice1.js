6e502515b1c7b49b20ba8631eda9d1f75a96139e
Kenneth <kennethyoung.2960@gmail.com>
2024-02-25 18:36:07 -0600

    Expanded const section for printTitlePriceAndAuthor function

--- practice1.js
+++ practice1.js
@@ -1,5 +1,6 @@
 const book = {
     title: "Nice Books to Read",
+    genre: "Essay",
     priceInCents: 1199,
     author: {
         firstName: "Phillip",
@@ -19,5 +20,16 @@ function printTitleAndPrice(product) {
 }
 //prints title and price without multiple const declarations and in one line. Also reduces usage of product.
 
+function printTitlePriceAndAuthor(product) {
+    const {
+        author: { firstName, lastName },
+        title,
+        priceInCents
+    } = product;
+
+    return `The book ${title} by ${firstName} ${lastName} is available for $${(priceInCents / 100).toFixed(2)}` // 'The book "Title" by "Author" is available for purchase for "Price" 
+}
+
 console.log(printTitleAndPrice(book));
 console.log(printAuthorAndTitle(book));
+console.log(printTitlePriceAndAuthor(book));
